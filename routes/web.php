8e347fa5c5c63167db270bfee3bdf9187b412200
Nilesh Bhardwaj <testuser1991998@gmail.com>
2023-11-16 15:41:45 +0530

    Added changes in the web.php

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ use App\Http\Controllers\PageSpeedController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+|i am added this comment here and i want to check that the my git is workung or not
 */
+
+
 
 Route::get('/', function () {
     return view('welcome');
